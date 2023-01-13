4ca61b49731c6b8ef7d1cd1d4077203017a9b2d6
Jiali <98071264+jhan125@users.noreply.github.com>
2023-01-13 09:30:43 -0800

    Add some blank spaces between comments

--- Lab1/src/Employee.java
+++ Lab1/src/Employee.java
@@ -1,6 +1,7 @@
 /**
  * Name: Jiali Han
  * Lab 01
+ *
  * This is an Employee class that represents a single hourly employee.
  * Employee has a name (String), employee ID (String), the number of hours worked in a given week (double), and pay rate (double).
  */
@@ -15,6 +16,7 @@ public class Employee {
   /**
    * This constructor takes a name, ID, and pay rate as parameters and
    * sets the instance values appropriately.
+   *
    * @param String name the name of this Employee
    * @param String ID   the ID of this Employee
    * @param double rate the rate of this Employee
@@ -29,6 +31,7 @@ public class Employee {
   /**
    * This method takes a parameter (double) and adds the value of that parameter
    * to the current number of hours the Employee has worked this week.
+   *
    * @param double workingHours the working hours of this Employee
    */
   public void addHoursWorked(double workingHours) {
@@ -40,13 +43,13 @@ public class Employee {
   /**
    * This method resets the employee’s hours worked for the week to zero.
    */
-  // this method
   public void resetHoursWorked() {
     this.hours = 0;
   }
 
   /**
    * This method returns (double) representing the hours worked for the employee.
+   *
    * @return double hours the working hours of this Employee
    */
   public double getHoursWorked() {
@@ -55,6 +58,7 @@ public class Employee {
 
   /**
    * This method returns a new Paycheck object that is initialized with rate and hours worked by the Employee.
+   *
    * @return Paycheck abc_paycheck the paycheck object initialized with rate and hours worked by this Employee
    */
   public Paycheck getWeeklyCheck() {
@@ -65,6 +69,7 @@ public class Employee {
   /**
    * This method returns a String allowing Employee objects to be represented
    * by the employee name, ID, and current week's payment after taxes are assessed.
+   *
    * @return String output a String allowing Employee objects to be represented
    */
   public String toString() {
